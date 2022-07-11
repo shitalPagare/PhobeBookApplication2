3200c14c21173cf6002ed3b56d10539926f6f2c8
shitalP13 <skpagare1947@gmail.com>
2022-07-11 22:35:52 +0530

    PHNBKPPLCT-2 : Create SpringBootProject with Required dependencies

---
+++ src/main/java/com/BikkadIT/PhoneBookApplication2/PhoneBookApplication2Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.PhoneBookApplication2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApplication2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/PhoneBookApplication2/PhoneBookApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.PhoneBookApplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
